ade8e0bf41e9fc196b1dd007f67bc783170497f4
ca19571 <60776296+ca19571@users.noreply.github.com>
2020-11-17 15:42:09 +0000

    Update player_with_controls.dart

--- lib/src/player_with_controls.dart
+++ lib/src/player_with_controls.dart
@@ -9,22 +9,8 @@ import 'package:video_player/video_player.dart';
 
 class PlayerWithControls extends StatelessWidget {
   final bool isRotated;
+
   PlayerWithControls({@required this.isRotated, Key key}) : super(key: key);
-
-  Widget getVideoWidget(BuildContext context, Orientation orientation) {
-    Widget result;
-
-    result = OrientationBuilder(
-      builder: (context, orientation) {
-        return RotatedBox(
-          quarterTurns: isRotated? 2 : 0,
-          child: VideoPlayer(chewieController.videoPlayerController),
-        );
-      },
-    );
-
-    return result == null ? Container() : result;
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,28 +18,34 @@ class PlayerWithControls extends StatelessWidget {
 
     return Center(
       child: Container(
-        width: MediaQuery.of(context).size.width,
+        width: MediaQuery
+            .of(context)
+            .size
+            .width,
         child: AspectRatio(
           aspectRatio:
-              chewieController.aspectRatio ?? _calculateAspectRatio(context),
+          chewieController.aspectRatio ?? _calculateAspectRatio(context),
           child: _buildPlayerWithControls(chewieController, context),
         ),
       ),
     );
   }
 
-  Container _buildPlayerWithControls(
-      ChewieController chewieController, BuildContext context) {
-    print(222222222222);
+  Container _buildPlayerWithControls(ChewieController chewieController,
+      BuildContext context) {
     return Container(
       child: Stack(
         children: <Widget>[
           chewieController.placeholder ?? Container(),
           Center(
             child: AspectRatio(
-              aspectRatio: chewieController.aspectRatio ??
-                  _calculateAspectRatio(context),
-              child: getVideoWidget(context, orientation);
+                aspectRatio: chewieController.aspectRatio ??
+                    _calculateAspectRatio(context),
+                child: RotatedBox(
+                  quarterTurns: isRotated &&
+                      orientation != Orientation.landscape ? 2 : 0,
+                  child: VideoPlayer(chewieController.videoPlayerController),
+                );
             ),
           ),
           chewieController.overlay ?? Container(),
@@ -63,24 +55,26 @@ class PlayerWithControls extends StatelessWidget {
     );
   }
 
-  Widget _buildControls(
-    BuildContext context,
-    ChewieController chewieController,
-  ) {
+  Widget _buildControls(BuildContext context,
+      ChewieController chewieController,) {
     return chewieController.showControls
         ? chewieController.customControls != null
-            ? chewieController.customControls
-            : Theme.of(context).platform == TargetPlatform.android
-                ? MaterialControls()
-                : CupertinoControls(
-                    backgroundColor: Color.fromRGBO(41, 41, 41, 0.7),
-                    iconColor: Color.fromARGB(255, 200, 200, 200),
-                  )
+        ? chewieController.customControls
+        : Theme
+        .of(context)
+        .platform == TargetPlatform.android
+        ? MaterialControls()
+        : CupertinoControls(
+      backgroundColor: Color.fromRGBO(41, 41, 41, 0.7),
+      iconColor: Color.fromARGB(255, 200, 200, 200),
+    )
         : Container();
   }
 
   double _calculateAspectRatio(BuildContext context) {
-    final size = MediaQuery.of(context).size;
+    final size = MediaQuery
+        .of(context)
+        .size;
     final width = size.width;
     final height = size.height;
 
